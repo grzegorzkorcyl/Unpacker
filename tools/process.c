2a315e53e994267d0df8895fa52a2d3ad3568c05
Grzegorz Korcyl <grzegorz@linux-6mep.site>
2011-07-31 08:00:13 -0400

    added configurable window for hits

--- tools/process.c
+++ tools/process.c
@@ -1,6 +1,6 @@
 #include <iostream>
 
-int process(const char* filename, int evtNum, const char* subId, int ref, const char* endp)
+int process(const char* filename, int evtNum, const char* subId, int ref, const char* endp, int minWin, int maxWin)
 {
-	Unpacker un(filename, evtNum, subId, ref, endp);
+	Unpacker un(filename, evtNum, subId, ref, endp, minWin, maxWin);
 }
--- tools/process.pl
+++ tools/process.pl
@@ -8,6 +8,8 @@ my $evtNum = $ARGV[1];
 my $subId  = $ARGV[2];
 my $refCh  = $ARGV[3];
 my $endp   = $ARGV[4];
+my $minWin = $ARGV[5];
+my $maxWin = $ARGV[6];
 
 
 my @threads;
@@ -23,7 +25,12 @@ my $size = scalar(@data);
 for(my $i = 0; $i < $size; $i++) {
 	print "Creating thread for $data[$i]\n\n";
 
-	$threads[$i] = threads->create(sub { `root -l -b -q 'process.c("$data[$i]", $evtNum, "$subId", $refCh, "$endp")'`});
+	if (!defined $minWin || !defined $maxWin) {
+		$threads[$i] = threads->create(sub { `root -l -b -q 'process.c("$data[$i]", $evtNum, "$subId", $refCh, "$endp", -100000, -100000)'`});
+	}
+	else {
+		$threads[$i] = threads->create(sub { `root -l -b -q 'process.c("$data[$i]", $evtNum, "$subId", $refCh, "$endp", $minWin, $maxWin)'`});
+	}
 }
 
 for(my $i = 0; $i < $size; $i++) {
