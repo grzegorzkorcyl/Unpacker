1eddac126ac8c1b3f1c98d661d90396a29497bdd
grzegorzkorcyl <grzegorz.korcyl@gmail.com>
2012-02-21 14:24:20 +0100

    Update tools/process.c

--- tools/process.c
+++ tools/process.c
@@ -1,6 +1,6 @@
 #include <iostream>
 
-int process(const char* filename, int evtNum, const char* subId, int ref, const char* endp, int minWin, int maxWin)
+int process(const char* filename, int evtNum, const char* subId, int ref, const char* endp, int minWin, int maxWin, int quietMode, int fullSetup, int vhrMode)
 {
-	Unpacker un(filename, evtNum, subId, ref, endp, minWin, maxWin);
+	Unpacker un(filename, evtNum, subId, ref, endp, minWin, maxWin, quietMode, fullSetup, vhrMode);
 }
